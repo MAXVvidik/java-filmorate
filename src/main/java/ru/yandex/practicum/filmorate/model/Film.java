80b4eb87e054beb1722080639ea857d37ef88d20
MAXVvidik <95582672+MAXVvidik@users.noreply.github.com>
2022-08-11 23:37:30 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,17 +3,22 @@ package ru.yandex.practicum.filmorate.model;
 import java.time.LocalDate;
 import java.time.Duration;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
     private int id;//целочисленный идентификатор — id;
+    @NonNull
     private String name;//название — name;
+    @NonNull
     private String description;//описание — description;
+    @NonNull
     private LocalDate releaseDate;//дата релиза — releaseDate;
+    @NonNull
     private int duration;//продолжительность фильма — duration.
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,15 +2,20 @@ package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private int id;//целочисленный идентификатор — id;
+    @NonNull
     private String email; //электронная почта — email;
+    @NonNull
     private String login;//логин пользователя — login;
+    @NonNull
     private String name;//имя для отображения — name;
+    @NonNull
     private LocalDate birthday;//дата рождения — birthday.
 }
